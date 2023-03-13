91dfb990720b07ef2878c75f0ee8dbc4d5c55ba8
ariemax <aribprasetyo88@gmail.com>
2023-03-13 15:10:50 +0700

    pendahuluan CI

---
+++ myapp/application/views/percobaan.php
@@ -0,0 +1,5 @@
+<h1>Ini Halaman Percobaan</h1>
+
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error animi similique reprehenderit rem aperiam odio laboriosam obcaecati cumque. Magni quam esse eveniet excepturi, tenetur fuga praesentium nihil dicta amet voluptatum!</p>
+
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, molestiae deleniti nam debitis laborum vitae ipsum exercitationem optio deserunt esse commodi asperiores quo ipsam! Itaque ullam est officiis doloremque magni?</p>
